5ca9f52d50c69df2deecc39f2107cb3253a31f9b
Joe LaRue <jlarue617@gmail.com>
2019-06-30 15:32:34 -0400

    added country test

--- src/BuilderTestSample/Services/OrderService.cs
+++ src/BuilderTestSample/Services/OrderService.cs
@@ -3,60 +3,60 @@ using BuilderTestSample.Model;
 
 namespace BuilderTestSample.Services
 {
-    public class OrderService
+  public class OrderService
+  {
+    public void PlaceOrder(Order order)
     {
-        public void PlaceOrder(Order order)
-        {
-            ValidateOrder(order);
+      ValidateOrder(order);
 
-            ExpediteOrder(order);
+      ExpediteOrder(order);
 
-            AddOrderToCustomerHistory(order);
-        }
-
-        private void ValidateOrder(Order order)
-        {
-            if (order.Id != 0) throw new InvalidOrderException("Order ID must be 0.");
-            if (order.TotalAmount <= 0) throw new InvalidOrderException("Order amount must not be <= 0.");
-            if (order.Customer == null ) throw new InvalidOrderException("Order must have a customer");
-            ValidateCustomer(order.Customer);
-        }
-
-        private void ValidateCustomer(Customer customer)
-        {
-            if(customer.Id <= 0) throw new InvalidCustomerException("Customer.ID must not be <= 0");
-            if(customer.HomeAddress == null ) throw new InvalidCustomerException("Customer address must not be null");  
-            if(string.IsNullOrEmpty(customer.FirstName) || string.IsNullOrEmpty(customer.LastName)) 
-            {
-                throw new InvalidCustomerException("Customer First and Last name are required");
-            }
-                
-            if(customer.CreditRating <= 200) throw new InsufficientCreditException("Customer Credit rating must be > 200");
-            if(customer.TotalPurchases < 0) throw new InvalidCustomerException("Customer.TOtalPurchases must not be < 0");
-
-            ValidateAddress(customer.HomeAddress);
-        }
-
-        private void ValidateAddress(Address homeAddress)
-        {
-            if(string.IsNullOrEmpty(homeAddress.Street1)) throw new InvalidAddressException("Address street 1 must have a value!");
-            if(string.IsNullOrEmpty(homeAddress.City)) throw new InvalidAddressException("City must have a value!");
-             if(string.IsNullOrEmpty(homeAddress.State)) throw new InvalidAddressException("state must have a value!");
-            if(string.IsNullOrEmpty(homeAddress.PostalCode)) throw new InvalidAddressException("PostalCode must have a value!");
-            
-            // TODO: country is required (not null or empty)
-        }
-
-        private void ExpediteOrder(Order order)
-        {
-            // TODO: if credit rating > 500 and total purchases > 5000 set IsExpedited to true
-        }
-
-        private void AddOrderToCustomerHistory(Order order)
-        {
-            // TODO: add the order to the customer
-
-            // TODO: update the customer's total purchases property
-        }
+      AddOrderToCustomerHistory(order);
     }
+
+    private void ValidateOrder(Order order)
+    {
+      if (order.Id != 0) throw new InvalidOrderException("Order ID must be 0.");
+      if (order.TotalAmount <= 0) throw new InvalidOrderException("Order amount must not be <= 0.");
+      if (order.Customer == null) throw new InvalidOrderException("Order must have a customer");
+      ValidateCustomer(order.Customer);
+    }
+
+    private void ValidateCustomer(Customer customer)
+    {
+      if (customer.Id <= 0) throw new InvalidCustomerException("Customer.ID must not be <= 0");
+      if (customer.HomeAddress == null) throw new InvalidCustomerException("Customer address must not be null");
+      if (string.IsNullOrEmpty(customer.FirstName) || string.IsNullOrEmpty(customer.LastName))
+      {
+        throw new InvalidCustomerException("Customer First and Last name are required");
+      }
+
+      if (customer.CreditRating <= 200) throw new InsufficientCreditException("Customer Credit rating must be > 200");
+      if (customer.TotalPurchases < 0) throw new InvalidCustomerException("Customer.TOtalPurchases must not be < 0");
+
+      ValidateAddress(customer.HomeAddress);
+    }
+
+    private void ValidateAddress(Address homeAddress)
+    {
+      if (string.IsNullOrEmpty(homeAddress.Street1)) throw new InvalidAddressException("Address street 1 must have a value!");
+      if (string.IsNullOrEmpty(homeAddress.City)) throw new InvalidAddressException("City must have a value!");
+      if (string.IsNullOrEmpty(homeAddress.State)) throw new InvalidAddressException("state must have a value!");
+      if (string.IsNullOrEmpty(homeAddress.PostalCode)) throw new InvalidAddressException("PostalCode must have a value!");
+      if (string.IsNullOrEmpty(homeAddress.Country)) throw new InvalidAddressException("country must have a value!");
+
+    }
+
+    private void ExpediteOrder(Order order)
+    {
+      // TODO: if credit rating > 500 and total purchases > 5000 set IsExpedited to true
+    }
+
+    private void AddOrderToCustomerHistory(Order order)
+    {
+      // TODO: add the order to the customer
+
+      // TODO: update the customer's total purchases property
+    }
+  }
 }
--- src/BuilderTestSample/Tests/OrderServicePlaceOrder.cs
+++ src/BuilderTestSample/Tests/OrderServicePlaceOrder.cs
@@ -6,220 +6,244 @@ using Xunit;
 
 namespace BuilderTestSample.Tests
 {
-    public class OrderServicePlaceOrder
+  public class OrderServicePlaceOrder
+  {
+    private readonly OrderService _orderService = new OrderService();
+    private readonly OrderBuilder _orderBuilder = new OrderBuilder();
+    private readonly CustomerBuilder _customerBuilder = new CustomerBuilder();
+    private readonly AddressBuilder _addressBuilder = new AddressBuilder();
+
+    [Fact]
+    public void ThrowsExceptionGivenOrderWithExistingId()
     {
-        private readonly OrderService _orderService = new OrderService();
-        private readonly OrderBuilder _orderBuilder = new OrderBuilder();
-        private readonly CustomerBuilder _customerBuilder = new CustomerBuilder();
-        private readonly AddressBuilder _addressBuilder = new AddressBuilder();
+      var order = _orderBuilder
+                      .WithTestValues()
+                      .Id(123)
+                      .Build();
 
-        [Fact]
-        public void ThrowsExceptionGivenOrderWithExistingId()
-        {
-            var order = _orderBuilder
-                            .WithTestValues()
-                            .Id(123)
-                            .Build();
-
-            Assert.Throws<InvalidOrderException>(() => _orderService.PlaceOrder(order));
-        }
-
-        [Fact]
-         public void ThrowsExceptionGivenOrderWithZeroAmount()
-        {
-            var order = _orderBuilder
-                            .WithTestValues()
-                            .Amount(0)
-                            .Build();
-
-            Assert.Throws<InvalidOrderException>(() => _orderService.PlaceOrder(order));
-        }
-
-        [Fact]
-         public void ThrowsExceptionGivenOrderWithNullCustomer()
-        {
-            var order = _orderBuilder
-                            .WithTestValues()
-                            .Customer(null)
-                            .Build();
-
-            Assert.Throws<InvalidOrderException>(() => _orderService.PlaceOrder(order));
-        }
-
-        [Fact]
-         public void ThrowsExceptionGivenCustomerWithZeroId()
-        {
-            var customer = _customerBuilder.WithTestValues().Id(0).Build();
-            var order = _orderBuilder
-                            .WithTestValues()
-                            .Customer(customer)
-                            .Build();
-
-            Assert.Throws<InvalidCustomerException>(() => _orderService.PlaceOrder(order));
-        }
-
-          [Fact]
-         public void ThrowsExceptionGivenCustomerWithNullAddress()
-        {
-            var customer = _customerBuilder
-                                .WithTestValues()
-                                .Address(null)
-                                .Build();
-
-            var order = _orderBuilder
-                            .WithTestValues()
-                            .Customer(customer)
-                            .Build();
-
-            Assert.Throws<InvalidCustomerException>(() => _orderService.PlaceOrder(order));
-        }
-
-         [Fact]
-         public void ThrowsExceptionGivenCustomerWithNoFirstAndLastName()
-        {
-            var customer = _customerBuilder
-                                .WithTestValues()
-                                .FirstName("")
-                                .LastName("")
-                                .Build();
-
-            var order = _orderBuilder
-                            .WithTestValues()
-                            .Customer(customer)
-                            .Build();
-
-            Assert.Throws<InvalidCustomerException>(() => _orderService.PlaceOrder(order));
-        }
-
-        [Fact]
-         public void ThrowsExceptionGivenCustomerWithCreditRatingUnder200()
-        {
-            var customer = _customerBuilder
-                                .WithTestValues()
-                                .CreditRating(200)
-                                .Build();
-
-            var order = _orderBuilder
-                            .WithTestValues()
-                            .Customer(customer)
-                            .Build();
-
-            Assert.Throws<InsufficientCreditException>(() => _orderService.PlaceOrder(order));
-        }
-
-         [Fact]
-         public void ThrowsExceptionGivenCustomerWithTOtalPurchasesLessThan0()
-        {
-            var customer = _customerBuilder
-                                .WithTestValues()
-                                .TotalPurchases(-1)
-                                .Build();
-
-            var order = _orderBuilder
-                            .WithTestValues()
-                            .Customer(customer)
-                            .Build();
-
-            Assert.Throws<InvalidCustomerException>(() => _orderService.PlaceOrder(order));
-        }   
-
-         [Theory]
-         [InlineData("")]
-         [InlineData(null)]
-         public void ThrowsExceptionGivenAddressHasEmptyStreet1(string street1)
-        {
-                                
-            var address = _addressBuilder
-                            .WithTestValues()
-                            .Street1(street1)
-                            .Build();
-
-            var customer = _customerBuilder
-                                .WithTestValues()
-                                .Address(address)
-                                .Build();            
-
-            var order = _orderBuilder
-                            .WithTestValues()
-                            .Customer(customer)
-                            .Build();
-
-            Assert.Throws<InvalidAddressException>(() => _orderService.PlaceOrder(order));
-        }   
-
-         [Theory]
-         [InlineData("")]
-         [InlineData(null)]
-         public void ThrowsExceptionGivenAddressHasEmptycity(string city)
-        {
-                                
-            var address = _addressBuilder
-                            .WithTestValues()
-                            .City(city)
-                            .Build();
-
-            var customer = _customerBuilder
-                                .WithTestValues()
-                                .Address(address)
-                                .Build();            
-
-            var order = _orderBuilder
-                            .WithTestValues()
-                            .Customer(customer)
-                            .Build();
-
-            Assert.Throws<InvalidAddressException>(() => _orderService.PlaceOrder(order));
-        }   
-
-         [Theory]
-         [InlineData("")]
-         [InlineData(null)]
-         public void ThrowsExceptionGivenAddressHasEmptyPostalCode(string postalCode)
-        {
-                                
-            var address = _addressBuilder
-                            .WithTestValues()
-                            .PostalCode(postalCode)
-                            .Build();
-
-            var customer = _customerBuilder
-                                .WithTestValues()
-                                .Address(address)
-                                .Build();            
-
-            var order = _orderBuilder
-                            .WithTestValues()
-                            .Customer(customer)
-                            .Build();
-
-            Assert.Throws<InvalidAddressException>(() => _orderService.PlaceOrder(order));
-        }  
-
- [Theory]
-         [InlineData("")]
-         [InlineData(null)]
-         public void ThrowsExceptionGivenAddressHasEmptyState(string state)
-        {
-                                
-            var address = _addressBuilder
-                            .WithTestValues()
-                            .State(state)
-                            .Build();
-
-            var customer = _customerBuilder
-                                .WithTestValues()
-                                .Address(address)
-                                .Build();            
-
-            var order = _orderBuilder
-                            .WithTestValues()
-                            .Customer(customer)
-                            .Build();
-
-            Assert.Throws<InvalidAddressException>(() => _orderService.PlaceOrder(order));
-        }  
-
-       
+      Assert.Throws<InvalidOrderException>(() => _orderService.PlaceOrder(order));
     }
+
+    [Fact]
+    public void ThrowsExceptionGivenOrderWithZeroAmount()
+    {
+      var order = _orderBuilder
+                      .WithTestValues()
+                      .Amount(0)
+                      .Build();
+
+      Assert.Throws<InvalidOrderException>(() => _orderService.PlaceOrder(order));
+    }
+
+    [Fact]
+    public void ThrowsExceptionGivenOrderWithNullCustomer()
+    {
+      var order = _orderBuilder
+                      .WithTestValues()
+                      .Customer(null)
+                      .Build();
+
+      Assert.Throws<InvalidOrderException>(() => _orderService.PlaceOrder(order));
+    }
+
+    [Fact]
+    public void ThrowsExceptionGivenCustomerWithZeroId()
+    {
+      var customer = _customerBuilder.WithTestValues().Id(0).Build();
+      var order = _orderBuilder
+                      .WithTestValues()
+                      .Customer(customer)
+                      .Build();
+
+      Assert.Throws<InvalidCustomerException>(() => _orderService.PlaceOrder(order));
+    }
+
+    [Fact]
+    public void ThrowsExceptionGivenCustomerWithNullAddress()
+    {
+      var customer = _customerBuilder
+                          .WithTestValues()
+                          .Address(null)
+                          .Build();
+
+      var order = _orderBuilder
+                      .WithTestValues()
+                      .Customer(customer)
+                      .Build();
+
+      Assert.Throws<InvalidCustomerException>(() => _orderService.PlaceOrder(order));
+    }
+
+    [Fact]
+    public void ThrowsExceptionGivenCustomerWithNoFirstAndLastName()
+    {
+      var customer = _customerBuilder
+                          .WithTestValues()
+                          .FirstName("")
+                          .LastName("")
+                          .Build();
+
+      var order = _orderBuilder
+                      .WithTestValues()
+                      .Customer(customer)
+                      .Build();
+
+      Assert.Throws<InvalidCustomerException>(() => _orderService.PlaceOrder(order));
+    }
+
+    [Fact]
+    public void ThrowsExceptionGivenCustomerWithCreditRatingUnder200()
+    {
+      var customer = _customerBuilder
+                          .WithTestValues()
+                          .CreditRating(200)
+                          .Build();
+
+      var order = _orderBuilder
+                      .WithTestValues()
+                      .Customer(customer)
+                      .Build();
+
+      Assert.Throws<InsufficientCreditException>(() => _orderService.PlaceOrder(order));
+    }
+
+    [Fact]
+    public void ThrowsExceptionGivenCustomerWithTOtalPurchasesLessThan0()
+    {
+      var customer = _customerBuilder
+                          .WithTestValues()
+                          .TotalPurchases(-1)
+                          .Build();
+
+      var order = _orderBuilder
+                      .WithTestValues()
+                      .Customer(customer)
+                      .Build();
+
+      Assert.Throws<InvalidCustomerException>(() => _orderService.PlaceOrder(order));
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void ThrowsExceptionGivenAddressHasEmptyStreet1(string street1)
+    {
+
+      var address = _addressBuilder
+                      .WithTestValues()
+                      .Street1(street1)
+                      .Build();
+
+      var customer = _customerBuilder
+                          .WithTestValues()
+                          .Address(address)
+                          .Build();
+
+      var order = _orderBuilder
+                      .WithTestValues()
+                      .Customer(customer)
+                      .Build();
+
+      Assert.Throws<InvalidAddressException>(() => _orderService.PlaceOrder(order));
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void ThrowsExceptionGivenAddressHasEmptycity(string city)
+    {
+
+      var address = _addressBuilder
+                      .WithTestValues()
+                      .City(city)
+                      .Build();
+
+      var customer = _customerBuilder
+                          .WithTestValues()
+                          .Address(address)
+                          .Build();
+
+      var order = _orderBuilder
+                      .WithTestValues()
+                      .Customer(customer)
+                      .Build();
+
+      Assert.Throws<InvalidAddressException>(() => _orderService.PlaceOrder(order));
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void ThrowsExceptionGivenAddressHasEmptyPostalCode(string postalCode)
+    {
+
+      var address = _addressBuilder
+                      .WithTestValues()
+                      .PostalCode(postalCode)
+                      .Build();
+
+      var customer = _customerBuilder
+                          .WithTestValues()
+                          .Address(address)
+                          .Build();
+
+      var order = _orderBuilder
+                      .WithTestValues()
+                      .Customer(customer)
+                      .Build();
+
+      Assert.Throws<InvalidAddressException>(() => _orderService.PlaceOrder(order));
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void ThrowsExceptionGivenAddressHasEmptyState(string state)
+    {
+
+      var address = _addressBuilder
+                      .WithTestValues()
+                      .State(state)
+                      .Build();
+
+      var customer = _customerBuilder
+                          .WithTestValues()
+                          .Address(address)
+                          .Build();
+
+      var order = _orderBuilder
+                      .WithTestValues()
+                      .Customer(customer)
+                      .Build();
+
+      Assert.Throws<InvalidAddressException>(() => _orderService.PlaceOrder(order));
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void ThrowsExceptionGivenAddressHasEmptyCountry(string country)
+    {
+
+      var address = _addressBuilder
+                      .WithTestValues()
+                      .Country(country)
+                      .Build();
+
+      var customer = _customerBuilder
+                          .WithTestValues()
+                          .Address(address)
+                          .Build();
+
+      var order = _orderBuilder
+                      .WithTestValues()
+                      .Customer(customer)
+                      .Build();
+
+      Assert.Throws<InvalidAddressException>(() => _orderService.PlaceOrder(order));
+    }
+
+
+  }
 }
 
